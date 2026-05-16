7a2878948e7f258429dee1b8bf858ad503340f50
robinsonmangang4-rgb <robinsonmangang4@gmail.com>
2026-05-16 22:07:14 +0530

    Switch statement simple traffic program

---
+++ Traffic.java
@@ -0,0 +1,18 @@
+public class Traffic {
+
+    public static void main(String[] args) {
+
+        String light = "red";
+
+        switch(light) {
+
+            case "red" -> System.out.println("Stop");
+
+            case "yellow" -> System.out.println("Ready");
+
+            case "green" -> System.out.println("Go");
+
+            default -> System.out.println("Invalid signal");
+        }
+    }
+}
